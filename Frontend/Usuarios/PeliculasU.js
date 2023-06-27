a4c6c5529efbda0f05ac9bafd9fd3410a4207c00
Personal <mgtubac@gmail.com>
2023-06-27 17:02:06 -0600

    Primero

---
+++ Frontend/Usuarios/PeliculasU.js
@@ -0,0 +1,66 @@
+var objeto = { 'nombrePelicula': sessionStorage }
+//sessionStorage.peliculaVer
+getPelicula();
+getComentarios();
+
+
+function getPelicula() {
+    fetch(`http://localhost:3000/getPelicula`, {
+        method: 'POST',
+        body: JSON.stringify(objeto),
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+        }
+    })
+        .then(res => res.json())
+        .catch(err => {
+            console.error('Error:', err)
+            alert("Ocurrio un error")
+        })
+        .then(response => {
+            console.log(response)
+            pelicula = response.data
+            document.getElementById("PeliculasU-nombre").value = pelicula.nombre
+            document.getElementById("VerPelicula-genero").value = pelicula.genero
+            document.getElementById("VerPelicula-clasificacion").value = pelicula.clasificacion
+            document.getElementById("VerPelicula-anio").value = pelicula.anio
+            document.getElementById("VerPelicula-duracion").value = pelicula.duracion
+            document.getElementById("PeliculasU-link").value = pelicula.link
+            document.getElementById("video").src = pelicula.link
+            //document.getElementsByClassName("ideo").src=pelicula.link
+        })
+}
+
+function getComentarios() {
+    fetch(`http://localhost:3000/getComentarios`, {
+        method: 'POST',
+        body: JSON.stringify(objeto),
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+        }
+    })
+        .then(res => res.json())
+        .catch(err => {
+            console.error('Error:', err)
+            alert("Ocurrio un error")
+        })
+        .then(response => {
+            console.log(response)
+            actualizarTabla(response.data)
+        })
+}
+
+// function actualizarTabla(registros) {
+//     cadena = ``;
+//     registros.forEach(element => {
+//         cadena += `<tr>
+//             <td> ${element.autor} </td>
+//             <td> ${element.mensaje} </td>
+//             </tr>`
+//     });
+
+//     var tbody = document.getElementById('comentarios');
+//     tbody.innerHTML = cadena
+// }
